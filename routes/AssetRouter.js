02ef63201e1dfc0c156ec71cb10b5d132ada7c3b
Malak <malakalmahari7042@gmail.com>
2025-08-01 13:35:07 +0300

    added protected routes

--- routes/AssetRouter.js
+++ routes/AssetRouter.js
@@ -4,11 +4,26 @@ const middleware = require('../middleware')
 const maintenanceLogRouter = require('./maintenanceLogRouter')
 
 //routes for assets
-router.post("/", controller.CreateAsset)
-router.get("/", controller.GetAssets)
-router.get("/:assetid", controller.GetAssetByid)
-router.put("/:assetid", controller.UpdateAsset)
-router.delete("/:assetid", controller.DeleteAsset)
+router.post("/", 
+    middleware.stripToken,
+    middleware.verifyToken,
+    controller.CreateAsset)
+router.get("/", 
+    middleware.stripToken,
+    middleware.verifyToken,    
+    controller.GetAssets)
+router.get("/:assetid", 
+    middleware.stripToken,
+    middleware.verifyToken,    
+    controller.GetAssetByid)
+router.put("/:assetid", 
+    middleware.stripToken,
+    middleware.verifyToken,    
+    controller.UpdateAsset)
+router.delete("/:assetid", 
+    middleware.stripToken,
+    middleware.verifyToken,    
+    controller.DeleteAsset)
 
 //routes for maintenance
 router.use("/:assetid/maintenancelogs", maintenanceLogRouter)
--- routes/maintenanceLogRouter.js
+++ routes/maintenanceLogRouter.js
@@ -2,9 +2,21 @@ const router = require('express').Router({mergeParams: true})
 const maintenanceLogController = require('../controllers/maintenanceLogController');
 const middleware = require('../middleware')
 
-router.post('/', maintenanceLogController.AddMaintenanceLog)
-router.get('/', maintenanceLogController.GetAssetMaintenanceLogs)
-router.put('/:logid', maintenanceLogController.UpdateMaintenanceLog)
-router.delete('/:logid', maintenanceLogController.DeleteMaintenanceLog)
+router.post('/', 
+    middleware.stripToken,
+    middleware.verifyToken,    
+    maintenanceLogController.AddMaintenanceLog)
+router.get('/', 
+    middleware.stripToken,
+    middleware.verifyToken,    
+    maintenanceLogController.GetAssetMaintenanceLogs)
+router.put('/:logid', 
+    middleware.stripToken,
+    middleware.verifyToken,    
+    maintenanceLogController.UpdateMaintenanceLog)
+router.delete('/:logid', 
+    middleware.stripToken,
+    middleware.verifyToken,    
+    maintenanceLogController.DeleteMaintenanceLog)
 
 module.exports = router
